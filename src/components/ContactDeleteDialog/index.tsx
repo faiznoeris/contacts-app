5b92ea305195045472ca69ac6ad4e640c1165ce1
faiznoeris <m.faiznoeris@gmail.com>
2023-06-24 08:49:50 +0700

    feat: create dialog modal for delete contact

---
+++ src/components/ContactDeleteDialog/index.tsx
@@ -0,0 +1,55 @@
+import { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+import { AppDispatch } from '../../store';
+import { deleteContact, fetchContacts } from '../../store/contactSlice';
+
+interface ContactDeleteDialogProps {
+    open: boolean;
+    handleClose: () => void;
+    id: string | null;
+}
+
+const ContactDeleteDialog = (props: ContactDeleteDialogProps) => {
+    const { open, handleClose, id } = props;
+    const dispatch = useDispatch<AppDispatch>();
+
+    const handleDelete = useCallback(() => {
+        dispatch(deleteContact(id)).then(() => {
+            handleClose();
+            dispatch(fetchContacts);
+        });
+    }, [dispatch, handleClose, id]);
+
+    return (
+        <Dialog
+            open={open}
+            onClose={handleClose}
+            aria-labelledby='alert-dialog-title'
+            aria-describedby='alert-dialog-description'
+            fullWidth
+        >
+            <DialogTitle id='alert-dialog-title'>Delete Contact</DialogTitle>
+            <DialogContent>
+                <DialogContentText id='alert-dialog-description'>
+                    Are you sure you want to delete this contact?
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose}>Cancel</Button>
+                <Button onClick={handleDelete} autoFocus>
+                    Yes
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
+};
+
+export default ContactDeleteDialog;
