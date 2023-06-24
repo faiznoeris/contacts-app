a568b11ea60f7d2b5fa8e8d351c3b11788fd7e52
faiznoeris <m.faiznoeris@gmail.com>
2023-06-24 08:50:18 +0700

    feat: remove unused style on App.css and index.css and adjust App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,92 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
-function App() {
-  const [count, setCount] = useState(0)
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import AddIcon from '@mui/icons-material/Add';
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+import Navbar from './components/Navbar';
+import ContactList from './components/ContactList';
+import ContactModal from './components/ContactModal';
+import ContactDeleteDialog from './components/ContactDeleteDialog';
 
-export default App
+import { fetchContacts } from './store/contactSlice';
+import { AppDispatch, RootState } from './store';
+
+const HomePage = () => {
+    const data = useSelector((state: RootState) => state.contact);
+    const dispatch = useDispatch<AppDispatch>();
+
+    const [open, setOpen] = useState(false);
+    const [openDelete, setOpenDelete] = useState(false);
+    const [id, setId] = useState<string | null>(null);
+    const [idDelete, setIdDelete] = useState<string | null>(null);
+
+    const handleOpen = (userId: string | null) => () => {
+        setOpen(true);
+        if (userId) setId(userId);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+        setId(null);
+    };
+
+    const handleOpenDelete = (contactId: string | null) => () => {
+        setOpenDelete(true);
+        if (contactId) setIdDelete(contactId);
+    };
+
+    const handleCloseDelete = () => setOpenDelete(false);
+
+    useEffect(() => {
+        dispatch(fetchContacts());
+    }, [dispatch]);
+
+    return (
+        <Box
+            sx={{
+                width: '100%',
+                height: '100%',
+            }}
+        >
+            <Navbar />
+            <Box padding={8}>
+                <Grid mt={10} display='flex' justifyContent='space-between'>
+                    <Grid>
+                        <Typography variant='h3'>Contact List</Typography>
+                    </Grid>
+                    <Grid>
+                        <Button
+                            variant='contained'
+                            onClick={handleOpen(null)}
+                            startIcon={<AddIcon />}
+                        >
+                            Add New Contact
+                        </Button>
+                    </Grid>
+                </Grid>
+                <ContactList
+                    data={data.contacts}
+                    handleOpen={handleOpen}
+                    handleOpenDelete={handleOpenDelete}
+                    isLoading={data?.isListLoading}
+                />
+            </Box>
+            <ContactModal
+                handleClose={handleClose}
+                open={open}
+                id={id}
+            />
+            <ContactDeleteDialog
+                handleClose={handleCloseDelete}
+                open={openDelete}
+                id={idDelete}
+            />
+        </Box>
+    );
+};
+
+export default HomePage;
